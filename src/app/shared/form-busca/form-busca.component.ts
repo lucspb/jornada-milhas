5b397d52bcd72cfe7d262c1dc5b0f8f9e6412843
Lucas Estanislau <lucasestanislau@hotmail.com>
2025-08-19 19:32:16 -0300

    feat - 1a parte do form de busca

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormBuscaComponent {
+  
+}
